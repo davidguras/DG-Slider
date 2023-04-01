d5359ae0276708939e4c1b2abfdb0e712cefdc96
David Guras <david.guras@pm.me>
2023-03-31 20:32:20 -0400

    initial commit

---
+++ dg-slider.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Plugin Name: DG Slider
+ * Plugin URI: https://www.wordpress.org/mv-slider
+ * Description: Plugin description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: David Guras
+ * Author URI: https://www.wordpress.org
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: dg-slider
+ * Domain Path: /languages
+ */
+
+/*
+MV Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+MV Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MV Slider. If not, see {URI to Plugin License}.
+*/
+
+// The following line prevents direct access to the plugin file.
+defined('ABSPATH') or die('Direct access not allowed.');
